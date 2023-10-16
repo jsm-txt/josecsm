912a45c0006d9fa001f1e55f865f50c964456887
Jose <hose153@gmail.com>
2023-10-15 20:57:53 -0700

    Added more images, fixed skill list

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,6 +6,10 @@ import uwgb from './uwgb.jpg'
 import duc from './duc.jpg'
 import aec from './aec.jpg'
 import colab from './colab.png'
+import duolingo from './duolingo.jpeg'
+import coursera from './coursera.webp'
+import music from './music.png'
+import computer from './computer.jpg'
 
 const email = "josesm0415@gmail.com"
 // function copyToClipboard() {
@@ -47,13 +51,60 @@ function Portfolio() {
             <div class="container-fluid">
                 <h3> About Me </h3>
                 <div class="work-desc col-12 pb-4">
-                    A recent graduate from Dominican University of California with a Bachlors in Computer Sciece. 
-                    I've had 6 years worth of higher eduaction by taking the accelerated program from Dominican combined
-                    with my previous course work. I've worked on bringin out the most out of me and my work which
-                    can be seen on my github. I've collaborated with many other developers, fellow studnets and cleints 
-                    while bringing a lot of enthusiasm, motivation and ability to learn to the table. I look forward to 
-                    getting to know more people across the field as I discover more about the every growing nature of 
-                    Computer Science and the Web.
+                    A recent graduate from Dominican University of California with a Bachelor's in Computer Sciece. 
+                    I have 6 years worth of higher education including the accelerated program from Dominican University combined
+                    with my previous course work. I've collaborated with many other developers, fellow students, and clients 
+                    while bringing a lot of enthusiasm, motivation and will to learn to the table. I enjoy getting to know more people in the field and gaining experience that will allow me to grow as a developer.
+                </div>
+            </div>
+
+            <hr class="col-6"></hr>
+            <div class="container-fluid col-12">
+                <h3> Skills </h3>
+                <div class="row edu p-2">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                    <div class="edu-name">
+                            Technical Skills
+                        </div>
+                        <hr class="sline"></hr>
+                        <ul class="skill-desc">
+                            <li>JavaScript (Node.js, React.js, Express.js, D3.js Chai)</li>
+                            <li>Python (Flask, Pytest)</li>
+                            <li>Java</li>
+                            <li>Typescript</li>
+                            <li>Swift (SwiftUI, UIKit)</li>
+                            <li>Databases (SQL, MongoDB)</li>
+                            <li>Cloud Development (Azure, Familiar with AWS,)</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="edu-name">
+                            Personal Traits
+                        </div>
+                        <hr class="sline"></hr>
+                        <ul class="skill-desc">
+                            <li>Excellent Communication </li>
+                            <li>Team Player</li>
+                            <li>Fast Learner</li>
+                            <li>Strong Under Pressure</li>
+                            <li>Level Headed</li>
+                            <li>Immune to Stress</li>
+                            <li>Hard Worker</li>
+                        </ul>
+                    </div>
+                </div>
+               
+                <div class="edu-name col-12 pb-0">
+                    Other Skill Sets: 
+                </div>
+                <div class="work-desc col-12 pb-2">
+                    Agile Methodologies, Containerization, DevOps, Forklift Certified
+                </div>
+                <div class="edu-name col-12 pb-0">
+                    Languages: 
+                </div>
+                <div class="work-desc col-12 pb-4">
+                    English - Fluent, Spanish - Fluent, French - Intermediate
                 </div>
             </div>
             <hr class="col-6"></hr>
@@ -68,7 +119,7 @@ function Portfolio() {
                     <div class="col-sm-4 col-md-4 mt-2">
                         <div class="edu-name">Dominican University of California</div>
                         <div> Aug 2020 -  Dec 2022 </div>
-                        <div class="edu-desc">Dominican's Acccelerated Program with a Bachlors of Computer Science</div>
+                        <div class="edu-desc">Dominican's Acccelerated Program, Bachelor of Science, Computer Science</div>
                     </div>
                 </div>
                 <div class="row edu pb-4">
@@ -78,7 +129,7 @@ function Portfolio() {
                     <div class="col-sm-4 col-md-4 mt-2">
                         <div class="edu-name">University of Wisconsin - Green Bay</div>
                         <div> Sept 2018 -  May 2020</div>
-                        <div class="edu-desc"> Associate's Degree of Art's and Science</div>
+                        <div class="edu-desc"> Associate's Degree of Arts and Science</div>
                     </div>
                 </div>
             </div>
@@ -102,10 +153,11 @@ function Portfolio() {
                     An 8 week project in a team of 4 to research and pitch a software solution
                     for language learners. Performed market research, user journeys and viewed
                     analytics to determine the best solution.
-                    Led, planned and developed a working software solution in the small time
-                    frame exceeding our MVP deliverables. Created a dual-subtitle
-                    chrome extension that works on Netflix. Worked in a remote startup environment,
-                    collaborating with team members across the US.
+                    Worked in a remote startup environment,
+                    collaborating with team members across the US. 
+                    Led, planned and developed a working software solution in a small time
+                    frame while exceeding MVP standard of deliverables. Created a dual-subtitle
+                    chrome extension that works on Netflix. 
                     Built using Javascript and CSS and added user features
                     for customization and usability.
                     Landing page at https://mailchi.mp/f9063425d3aa/gaja.
@@ -124,13 +176,13 @@ function Portfolio() {
                     </div>
                 </div>
                 <div class="work-desc col-12 pb-3">
-                    A 12 week project in an out of a class setting in of 10 team to build a
+                    A 12 week project, out-of-class setting with a team of 10 to build a
                     web application for a client in the UK.
                     Created a social media application proof of concept and collaborated
-                    with client to fufill requirements.
-                    to help medical students reach out to professionals.
+                    with client to fufill requirement of
+                    helping medical students reach out to professionals.
                     Planned, designed, problem solved, and implemented routes, schemas,
-                    and Oauth for user security.
+                    and OAuth for user security.
                 </div>
                 <div class="row edu p-2">
                     <div class="col-sm-6 col-md-6">
@@ -141,11 +193,11 @@ function Portfolio() {
                     </div>
                 </div>
                 <div class="work-desc col-12 pb-3">
-                    A 8 week project in a team of 5 to solve housing selling and buying issues.
-                    Created a web application to that can find houses and give you an estimate
+                    An 8 week project in a team of 5 to solve housing selling and buying issues.
+                    Created a web application that can find houses and give you an estimate
                     on the value of your house using machine learning.
-                    As a lead frontend developer, worked on UI/UX concepts and applied simple
-                    to use UI and features.
+                    As a lead frontend developer, worked on UI/UX concepts and applied simple and easy-to-use
+                    UI and features.
                 </div>
                 <div class="row edu p-2">
                     <div class="col-sm-6 col-md-6">
@@ -156,14 +208,14 @@ function Portfolio() {
                     </div>
                 </div>
                 <div class="work-desc col-12 pb-3">
-                    A weather application built with react that gives you weather
-                    data for a daily and weekly forecast. Implemented D3.js to create
+                    A weather application built with React that gives you weather
+                    data for  daily and weekly forecasts. Implemented D3.js to create
                     a simple UI for convenience, interpretation, and readability of weather data.
                     Applied user tests, analytics, AB testing, and growth hacks to extend reachability to users.
                 </div>
                 <h4> Smaller projects</h4>
-                <div class="row edu p-2 pb-4">
-                    <div class=" col-4">
+                <div class="row edu p-2 pb-3">
+                    <div class=" col-sm-12 col-md-4 col-lg-4">
                         <div class="project-card h-100 p-3">
                             <div class="work-name">iOS Calculator App</div>
                             <div class="work-links">Swift</div>
@@ -171,7 +223,7 @@ function Portfolio() {
                             <div>I built an iOS calculator apps similar to the default one</div>
                         </div>
                     </div>
-                    <div class=" col-4">
+                    <div class=" col-sm-12 col-md-4 col-lg-4">
                         <div class="project-card h-100 p-3">
                             <div class="work-name">Eutheruem Contract</div>
                             <div class="work-links">Solidium</div>
@@ -180,6 +232,7 @@ function Portfolio() {
                         </div>
                     </div>
                 </div>
+                
                 {/* <div class="row edu p-2">
                     <div class="col-4">
                         <div class="project-card h-100 p-3">
@@ -198,12 +251,7 @@ function Portfolio() {
                         </div>
                     </div>
                 </div> */}
-            </div>
-            {/* <hr class="col-6"></hr>ß
-            <div class="container-fluid">
-            <h3>Hobbies </h3>
-
-            </div> */}
+            </div>    
 
             <hr class="col-6"></hr>
             <div class="container-fluid">
@@ -239,11 +287,59 @@ function Portfolio() {
                     </div>
                 </div>
                 <div class="work-desc col-12 pb-4">
-                    Line cook responsible for presparing food for customers in a timely fashion.
+                    Line cook responsible for preparing food for customers in a timely fashion.
                 </div>
             </div>
+
             <hr class="col-6"></hr>
+            <div class="container-fluid">
+                <h3> Hobbies/ Freetime</h3>
+                <div class="row edu p-2 pt-1">
+                    <div class="col-sm-12 col-md-4 col-lg-4 ">
+                        <img class="logo" src={coursera} alt="" />
+                        <div class="edu-name p-1">Skill development</div>
+                        <div>
+                            I am currently enrolled in a Coursera DevOps Professional Certificate course
+                            and working towards Azure Cloud Certification on Cloud Guru.
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <img class="logo" src={computer} alt="" />
+                        <div class="edu-name p-1">Working with computers</div>
+                        <div>
+                            I love working with computers. I currently run a
+                            side hustle of fixing and building computers for people in my area.
+                        </div>
+                    </div>
+                </div>
+                <div class="row edu p-2 pb-4">
+                    <div class="col-sm-12 col-md-4 col-lg-4 ">
+                        <img class="logo" src={music} alt="" />
+                        <div class="edu-name p-1">Music</div>
+                        <div> 
+                            I grew up playing violin and bass. Sometimes throughout the year I will go
+                            play music with a group. Places I have played include churches, nursing homes 
+                            and hospitals, just to name a few. 
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <img class="logo" src={duolingo} alt="" />
+                        <div class="edu-name p-1">Language Learning</div>
+                        <div>
+                            I was raised learning English and Spanish and it made me appreciate different cultures.
+                            Because of that and my 4 years of french in high school, I've continued my pursuit in
+                            becoming fluent and understanding more langugaes, such as French and Japanese with the help of Duolingo.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <hr class="col-6"></hr>            
         </div>
+        
 
     )
 }
